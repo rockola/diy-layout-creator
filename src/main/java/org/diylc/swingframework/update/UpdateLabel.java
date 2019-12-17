aae24ad50db0c97ff0683ab2b191fa7a3aa514e3
Ola Rinta-Koski <github@rinta-koski.net>
2019-12-17 21:23:51 +0200

    to UTF-8

--- src/main/java/org/diylc/swingframework/update/UpdateLabel.java
+++ src/main/java/org/diylc/swingframework/update/UpdateLabel.java
@@ -13,64 +13,66 @@ import org.diylc.appframework.update.UpdateChecker;
 import org.diylc.appframework.update.Version;
 import org.diylc.appframework.update.VersionNumber;
 
-
 public class UpdateLabel extends JLabel {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private UpdateChecker updateChecker;
-	private List<Version> updatedVersions;
+    private UpdateChecker updateChecker;
+    private List<Version> updatedVersions;
 
-	public UpdateLabel(final VersionNumber currentVersion, final String updateFileUrl) {
-		super();
-		updateChecker = new UpdateChecker(currentVersion, updateFileUrl);
+    public UpdateLabel(final VersionNumber currentVersion,
+		       final String updateFileUrl) {
+	super();
+	updateChecker = new UpdateChecker(currentVersion, updateFileUrl);
 
-		setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if ((updatedVersions != null) && (updatedVersions.size() > 0)) {
-					new UpdateDialog(UpdateLabel.this, UpdateChecker
-							.createUpdateHTML(updatedVersions), updatedVersions.get(0).getUrl())
-							.setVisible(true);
-				} else {
-					checkForUpdates();
-				}
+	setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+	addMouseListener(new MouseAdapter() {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+		    if ((updatedVersions != null) && (updatedVersions.size() > 0)) {
+			new UpdateDialog(UpdateLabel.this,
+					 UpdateChecker.createUpdateHTML(updatedVersions),
+					 updatedVersions.get(0).getUrl())
+			    .setVisible(true);
+		    } else {
+			checkForUpdates();
+		    }
+		}
+	    });
+	checkForUpdates();
+    }
+
+    private void checkForUpdates() {
+	SwingWorker<List<Version>, Void> worker =
+	    new SwingWorker<List<Version>, Void>() {
+
+		@Override
+		protected List<Version> doInBackground() throws Exception {
+		    return updateChecker.findNewVersions();
+		}
+
+		@Override
+		protected void done() {
+		    try {
+			updatedVersions = get();
+			if (updatedVersions.size() == 0) {
+			    setIcon(IconLoader.LightBulbOff.getIcon());
+			    setToolTipText("No updates available, click to check again");
+			} else {
+			    setIcon(IconLoader.LightBulbOn.getIcon());
+			    setToolTipText("Updates are available, click to see details");
 			}
-		});
-		checkForUpdates();
-	}
-
-	private void checkForUpdates() {
-		SwingWorker<List<Version>, Void> worker = new SwingWorker<List<Version>, Void>() {
-
-			@Override
-			protected List<Version> doInBackground() throws Exception {
-				return updateChecker.findNewVersions();
-			}
-
-			@Override
-			protected void done() {
-				try {
-					updatedVersions = get();
-					if (updatedVersions.size() == 0) {
-						setIcon(IconLoader.LightBulbOff.getIcon());
-						setToolTipText("No updates available, click to check again");
-					} else {
-						setIcon(IconLoader.LightBulbOn.getIcon());
-						setToolTipText("Updates are available, click to see details");
-					}
-				} catch (Exception e) {
-					setIcon(IconLoader.LightBulbOff.getIcon());
-					setToolTipText("Error occured while searching for updates: " + e.getMessage());
-					setCursor(Cursor.getDefaultCursor());
-				}
-			}
-		};
-		worker.execute();
-	}
+		    } catch (Exception e) {
+			setIcon(IconLoader.LightBulbOff.getIcon());
+			setToolTipText("Error occurred while searching for updates: " + e.getMessage());
+			setCursor(Cursor.getDefaultCursor());
+		    }
+		}
+	    };
+	worker.execute();
+    }
 	
-	public UpdateChecker getUpdateChecker() {
-      return updateChecker;
+    public UpdateChecker getUpdateChecker() {
+	return updateChecker;
     }
 }
