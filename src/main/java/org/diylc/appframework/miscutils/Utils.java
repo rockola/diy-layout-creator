e6b7699b34a99bdf42de48cc197d24bc723105dd
Ola Rinta-Koski <github@rinta-koski.net>
2019-12-18 13:17:12 +0200

    tidied up OS check functions

--- src/main/java/org/diylc/appframework/miscutils/Utils.java
+++ src/main/java/org/diylc/appframework/miscutils/Utils.java
@@ -87,29 +87,20 @@ public class Utils {
 	return clone;
     }
 
-    public static boolean isWindows() {
+    private static boolean checkOsName(String target) {
 	String os = System.getProperty("os.name").toLowerCase();
 	// windows
-	return (os.indexOf("win") >= 0);
+	return (os.indexOf(target) >= 0);
     }
 
-    public static boolean isMac() {
-	String os = System.getProperty("os.name").toLowerCase();
-	// Mac
-	return (os.indexOf("mac") >= 0);
-    }
-
+    public static boolean isWindows() { return checkOsName("win"); }
+    public static boolean isMac() { return checkOsName("mac"); }
+    public static boolean isSolaris() {	return checkOsName("sunos"); }
     public static boolean isUnix() {
-	String os = System.getProperty("os.name").toLowerCase();
 	// linux or unix
-	return (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0);
+	return (checkOsName("nix") || checkOsName("nux"));
     }
 
-    public static boolean isSolaris() {
-	String os = System.getProperty("os.name").toLowerCase();
-	// Solaris
-	return (os.indexOf("sunos") >= 0);
-    }
 
     public static Set<Class<?>> getClasses(String packageName) throws Exception {
 	ClassLoader loader = Thread.currentThread().getContextClassLoader();
