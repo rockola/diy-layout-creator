13b4ef3650a88f85697cb861bede70d7e39578b2
Ola Rinta-Koski <github@rinta-koski.net>
2019-12-18 10:29:44 +0200

    changed XStream references to use org.diylc.appframework.Serializer, indents

--- src/main/java/org/diylc/swing/plugins/config/ConfigPlugin.java
+++ src/main/java/org/diylc/swing/plugins/config/ConfigPlugin.java
@@ -1,19 +1,21 @@
 /*
- * 
- * DIY Layout Creator (DIYLC). Copyright (c) 2009-2018 held jointly by the individual authors.
+ * DIY Layout Creator (DIYLC). Copyright (c) 2009-2018 held jointly by
+ * the individual authors.
  * 
  * This file is part of DIYLC.
  * 
- * DIYLC is free software: you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * DIYLC is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
- * DIYLC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
- * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
- * Public License for more details.
+ * DIYLC is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along with DIYLC. If not, see
- * <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with DIYLC. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.diylc.swing.plugins.config;
 
@@ -32,9 +34,6 @@ import org.diylc.images.IconLoader;
 import org.diylc.swing.ActionFactory;
 import org.diylc.swing.ISwingUI;
 
-import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.DomDriver;
-
 /**
  * Controls configuration menu.
  * 
@@ -42,101 +41,104 @@ import com.thoughtworks.xstream.io.xml.DomDriver;
  */
 public class ConfigPlugin implements IPlugIn {
 
-  private static final Logger LOG = Logger.getLogger(ConfigPlugin.class);
+    private static final Logger LOG = Logger.getLogger(ConfigPlugin.class);
 
-  private static final String CONFIG_MENU = "Config";
-  private static final String THEME_MENU = "Theme";
-  private static final String COMPONENT_BROWSER_MENU = "Toolbox";
-  public static final String COMPONENT_BROWSER = "componentBrowser";
-  public static final String SEARCHABLE_TREE = "Searchable Tree";
-  public static final String TABBED_TOOLBAR = "Tabbed Toolbar";
+    private static final String CONFIG_MENU = "Config";
+    private static final String THEME_MENU = "Theme";
+    private static final String COMPONENT_BROWSER_MENU = "Toolbox";
+    public static final String COMPONENT_BROWSER = "componentBrowser";
+    public static final String SEARCHABLE_TREE = "Searchable Tree";
+    public static final String TABBED_TOOLBAR = "Tabbed Toolbar";
 
-  private ISwingUI swingUI;
+    private ISwingUI swingUI;
 
-  public ConfigPlugin(ISwingUI swingUI) {
-    super();
-    this.swingUI = swingUI;
-  }
-
-  @Override
-  public void connect(IPlugInPort plugInPort) {
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance()
-            .createConfigAction(plugInPort, "Anti-Aliasing", IPlugInPort.ANTI_ALIASING_KEY, true), CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Auto-Create Pads",
-            SolderPadAutoCreator.AUTO_PADS_KEY, false), CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Auto-Edit Mode", IPlugInPort.AUTO_EDIT_KEY, true),
-        CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Continuous Creation",
-            IPlugInPort.CONTINUOUS_CREATION_KEY, false), CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Export Grid", IPlugInPort.EXPORT_GRID_KEY, false),
-        CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Extra Working Area", IPlugInPort.EXTRA_SPACE_KEY, true),
-        CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Hardware Acceleration", IPlugInPort.HARDWARE_ACCELERATION, false),
-        CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Hi-Quality Rendering",
-            IPlugInPort.HI_QUALITY_RENDER_KEY, false), CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Highlight Connected Areas", IPlugInPort.HIGHLIGHT_CONTINUITY_AREA, 
-            false), CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Mouse Wheel Zoom", IPlugInPort.WHEEL_ZOOM_KEY,
-            false), CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Outline Mode", IPlugInPort.OUTLINE_KEY, false),
-        CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Show Rulers", IPlugInPort.SHOW_RULERS_KEY, true),
-        CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Show Grid", IPlugInPort.SHOW_GRID_KEY, true),
-        CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance().createConfigAction(plugInPort, "Snap to Grid", IPlugInPort.SNAP_TO_GRID_KEY, true),
-        CONFIG_MENU);
-    swingUI.injectMenuAction(
-        ActionFactory.getInstance()
-            .createConfigAction(plugInPort, "Sticky Points", IPlugInPort.STICKY_POINTS_KEY, true), CONFIG_MENU);
-
-    File themeDir = new File("themes");
-    if (themeDir.exists()) {
-      XStream xStream = new XStream(new DomDriver());
-      swingUI.injectSubmenu(THEME_MENU, IconLoader.Pens.getIcon(), CONFIG_MENU);
-      for (File file : themeDir.listFiles()) {
-        if (file.getName().toLowerCase().endsWith(".xml")) {
-          try {
-            InputStream in = new FileInputStream(file);
-            Theme theme = (Theme) xStream.fromXML(in);
-            LOG.debug("Found theme: " + theme.getName());
-            swingUI.injectMenuAction(ActionFactory.getInstance().createThemeAction(plugInPort, theme), THEME_MENU);
-          } catch (Exception e) {
-            LOG.error("Could not load theme file " + file.getName(), e);
-          }
-        }
-      }
+    public ConfigPlugin(ISwingUI swingUI) {
+	super();
+	this.swingUI = swingUI;
     }
 
-    swingUI.injectSubmenu(COMPONENT_BROWSER_MENU, IconLoader.Hammer.getIcon(), CONFIG_MENU);
+    @Override
+    public void connect(IPlugInPort plugInPort) {
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance()
+				 .createConfigAction(plugInPort, "Anti-Aliasing", IPlugInPort.ANTI_ALIASING_KEY, true), CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Auto-Create Pads",
+										SolderPadAutoCreator.AUTO_PADS_KEY, false), CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Auto-Edit Mode", IPlugInPort.AUTO_EDIT_KEY, true),
+				 CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Continuous Creation",
+										IPlugInPort.CONTINUOUS_CREATION_KEY, false), CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Export Grid", IPlugInPort.EXPORT_GRID_KEY, false),
+				 CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Extra Working Area", IPlugInPort.EXTRA_SPACE_KEY, true),
+				 CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Hardware Acceleration", IPlugInPort.HARDWARE_ACCELERATION, false),
+				 CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Hi-Quality Rendering",
+										IPlugInPort.HI_QUALITY_RENDER_KEY, false), CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Highlight Connected Areas", IPlugInPort.HIGHLIGHT_CONTINUITY_AREA, 
+										false), CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Mouse Wheel Zoom", IPlugInPort.WHEEL_ZOOM_KEY,
+										false), CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Outline Mode", IPlugInPort.OUTLINE_KEY, false),
+				 CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Show Rulers", IPlugInPort.SHOW_RULERS_KEY, true),
+				 CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Show Grid", IPlugInPort.SHOW_GRID_KEY, true),
+				 CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance().createConfigAction(plugInPort, "Snap to Grid", IPlugInPort.SNAP_TO_GRID_KEY, true),
+				 CONFIG_MENU);
+	swingUI.injectMenuAction(
+				 ActionFactory.getInstance()
+				 .createConfigAction(plugInPort, "Sticky Points", IPlugInPort.STICKY_POINTS_KEY, true), CONFIG_MENU);
 
-    swingUI.injectMenuAction(ActionFactory.getInstance().createComponentBrowserAction(SEARCHABLE_TREE),
-        COMPONENT_BROWSER_MENU);
-    swingUI.injectMenuAction(ActionFactory.getInstance().createComponentBrowserAction(TABBED_TOOLBAR),
-        COMPONENT_BROWSER_MENU);
-  }
+	File themeDir = new File("themes");
+	if (themeDir.exists()) {
+	    swingUI.injectSubmenu(THEME_MENU,
+				  IconLoader.Pens.getIcon(),
+				  CONFIG_MENU);
+	    for (File file : themeDir.listFiles()) {
+		if (file.getName().toLowerCase().endsWith(".xml")) {
+		    try {
+			Theme theme = (Theme) Serializer.fromFile(file);
+			LOG.debug("Found theme: " + theme.getName());
+			swingUI.injectMenuAction(ActionFactory.getInstance().createThemeAction(plugInPort, theme),
+						 THEME_MENU);
+		    } catch (Exception e) {
+			LOG.error("Could not load theme file " + file.getName(), e);
+		    }
+		}
+	    }
+	}
 
-  @Override
-  public EnumSet<EventType> getSubscribedEventTypes() {
-    return EnumSet.noneOf(EventType.class);
-  }
+	swingUI.injectSubmenu(COMPONENT_BROWSER_MENU,
+			      IconLoader.Hammer.getIcon(),
+			      CONFIG_MENU);
 
-  @Override
-  public void processMessage(EventType eventType, Object... params) {}
+	swingUI.injectMenuAction(ActionFactory.getInstance().createComponentBrowserAction(SEARCHABLE_TREE),
+				 COMPONENT_BROWSER_MENU);
+	swingUI.injectMenuAction(ActionFactory.getInstance().createComponentBrowserAction(TABBED_TOOLBAR),
+				 COMPONENT_BROWSER_MENU);
+    }
+
+    @Override
+    public EnumSet<EventType> getSubscribedEventTypes() {
+	return EnumSet.noneOf(EventType.class);
+    }
+
+    @Override
+    public void processMessage(EventType eventType, Object... params) {}
 }
