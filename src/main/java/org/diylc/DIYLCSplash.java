77072c430c81ebd244e8a93b2ea6702672e26ca9
Ola Rinta-Koski <github@rinta-koski.net>
2019-12-18 09:23:10 +0200

    IconLoader & to UTF-8

--- src/main/java/org/diylc/DIYLCSplash.java
+++ src/main/java/org/diylc/DIYLCSplash.java
@@ -11,105 +11,114 @@ import javax.swing.SwingUtilities;
 import org.diylc.images.IconLoader;
 
 public class DIYLCSplash {
-  
-  private Thread t;
 
-  public DIYLCSplash(final SplashScreen splash) {
-    if (splash == null)
-      return;
-    final Graphics2D g = splash.createGraphics();
-    if (g == null)
-      return;
+    private Thread t;
+
+    public DIYLCSplash(final SplashScreen splash) {
+	if (splash == null)
+	    return;
+	final Graphics2D g = splash.createGraphics();
+	if (g == null)
+	    return;
     
-    t = new Thread(new Runnable() {
+	t = new Thread(new Runnable() {
 
-      @Override
-      public void run() {
-        for (int i = 90; i >= 0; i--) {
-          if (!splash.isVisible())
-            return;
-          final int frame = i;
-          SwingUtilities.invokeLater(new Runnable() {
+		@Override
+		public void run() {
+		    for (int i = 90; i >= 0; i--) {
+			if (!splash.isVisible())
+			    return;
+			final int frame = i;
+			SwingUtilities.invokeLater(new Runnable() {
 
-            @Override
-            public void run() {
-              renderSplashFrame(splash, g, frame);
-              splash.update();
-            }
-          });
-          try {
-            Thread.sleep(10);
-          } catch (InterruptedException e) {
-          }
-        }
-      }
-    });    
-  }
+				@Override
+				public void run() {
+				    renderSplashFrame(splash, g, frame);
+				    splash.update();
+				}
+			    });
+			try {
+			    Thread.sleep(10);
+			} catch (InterruptedException e) {
+			}
+		    }
+		}
+	    });    
+    }
   
-  public void start() {
-    if (t != null)
-      t.start();
-  }
-
-  private ImageIcon resistor = null;
-
-  public ImageIcon getResistor() {
-    if (resistor == null) {
-      resistor = (ImageIcon) IconLoader.SplashResistor.getIcon();
+    public void start() {
+	if (t != null)
+	    t.start();
     }
-    return resistor;
-  }
 
-  private ImageIcon film = null;
+    private ImageIcon resistor = null;
 
-  public ImageIcon getFilm() {
-    if (film == null) {
-      film = (ImageIcon) IconLoader.SplashFilm.getIcon();
+    public ImageIcon getResistor() {
+	if (resistor == null) {
+	    resistor = (ImageIcon) IconLoader.SplashResistor.getIcon();
+	}
+	return resistor;
     }
-    return film;
-  }
 
-  private ImageIcon ceramic = null;
+    private ImageIcon film = null;
 
-  public ImageIcon getCeramic() {
-    if (ceramic == null) {
-      ceramic = (ImageIcon) IconLoader.SplashCeramic.getIcon();
+    public ImageIcon getFilm() {
+	if (film == null) {
+	    film = (ImageIcon) IconLoader.SplashFilm.getIcon();
+	}
+	return film;
     }
-    return ceramic;
-  }
 
-  private ImageIcon electrolytic = null;
+    private ImageIcon ceramic = null;
 
-  public ImageIcon getElectrolytic() {
-    if (electrolytic == null) {
-      electrolytic = (ImageIcon) IconLoader.SplashElectrolytic.getIcon();
+    public ImageIcon getCeramic() {
+	if (ceramic == null) {
+	    ceramic = (ImageIcon) IconLoader.SplashCeramic.getIcon();
+	}
+	return ceramic;
     }
-    return electrolytic;
-  }
 
-  private ImageIcon splash = null;
+    private ImageIcon electrolytic = null;
 
-  public ImageIcon getSplash() {
-    if (splash == null) {
-      splash = (ImageIcon) IconLoader.Splash.getIcon();
+    public ImageIcon getElectrolytic() {
+	if (electrolytic == null) {
+	    electrolytic = (ImageIcon) IconLoader.SplashElectrolytic.getIcon();
+	}
+	return electrolytic;
     }
-    return splash;
-  }
 
-  private Point resistorTarget = new Point(112, 114);
-  private Point filmTarget = new Point(233, 113);
-  private Point electrolyticTarget = new Point(261, 23);
-  private Point ceramicTarget = new Point(352, 22);
+    private ImageIcon splash = null;
 
-  private int pxPerFrame = 3;
+    public ImageIcon getSplash() {
+	if (splash == null) {
+	    splash = (ImageIcon) IconLoader.Splash.getIcon();
+	}
+	return splash;
+    }
 
-  public void renderSplashFrame(SplashScreen splash, Graphics2D g, int frame) {
-    g.setComposite(AlphaComposite.Clear);
-    getSplash().paintIcon(null, g, 0, 0);
-    g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.7f - frame * 0.007f));
-    getResistor().paintIcon(null, g, resistorTarget.x - pxPerFrame * frame, resistorTarget.y);
-    getFilm().paintIcon(null, g, filmTarget.x, filmTarget.y + pxPerFrame * frame);
-    getElectrolytic().paintIcon(null, g, electrolyticTarget.x, electrolyticTarget.y - pxPerFrame * frame);
-    getCeramic().paintIcon(null, g, ceramicTarget.x + pxPerFrame * frame, ceramicTarget.y);
-  }
+    private Point resistorTarget = new Point(112, 114);
+    private Point filmTarget = new Point(233, 113);
+    private Point electrolyticTarget = new Point(261, 23);
+    private Point ceramicTarget = new Point(352, 22);
+
+    private int pxPerFrame = 3;
+
+    public void renderSplashFrame(SplashScreen splash, Graphics2D g, int frame) {
+	g.setComposite(AlphaComposite.Clear);
+	getSplash().paintIcon(null, g, 0, 0);
+	g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,
+						  0.7f - frame * 0.007f));
+	getResistor().paintIcon(null, g,
+				resistorTarget.x - pxPerFrame * frame,
+				resistorTarget.y);
+	getFilm().paintIcon(null, g,
+			    filmTarget.x,
+			    filmTarget.y + pxPerFrame * frame);
+	getElectrolytic().paintIcon(null, g,
+				    electrolyticTarget.x,
+				    electrolyticTarget.y - pxPerFrame * frame);
+	getCeramic().paintIcon(null, g,
+			       ceramicTarget.x + pxPerFrame * frame,
+			       ceramicTarget.y);
+    }
 }
