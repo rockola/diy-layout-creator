ff5f0cd9dbd9048ae28069f7dff7129e04def9eb
Ola Rinta-Koski <github@rinta-koski.net>
2019-12-18 14:17:25 +0200

    show DIYLC++ instead of DIYLC in window title to differentiate between this and bancika version

--- src/main/java/org/diylc/swing/gui/MainFrame.java
+++ src/main/java/org/diylc/swing/gui/MainFrame.java
@@ -4,19 +4,18 @@
 
   This file is part of DIYLC.
 
-  DIYLC is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
+  DIYLC is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  DIYLC is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  DIYLC is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
 
   You should have received a copy of the GNU General Public License
   along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
-
 */
 package org.diylc.swing.gui;
 
@@ -446,7 +445,7 @@ public class MainFrame extends JFrame implements ISwingUI {
 		    fileName = "Untitled";
 		}
 		String modified = (Boolean) params[1] ? " (modified)" : "";
-		setTitle(String.format("DIYLC G3 version %s.%s - %s %s",
+		setTitle(String.format("DIYLC++ G3 version %s.%s - %s %s",
 				       plugInPort.getCurrentVersionNumber().getMinor(),
 				       plugInPort.getCurrentVersionNumber().getBuild(),
 				       fileName,
