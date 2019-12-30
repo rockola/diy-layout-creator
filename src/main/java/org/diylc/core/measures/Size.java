219a9a1fb139269da6ffebaef2ab04650594ec58
Ola Rinta-Koski <github@rinta-koski.net>
2019-12-30 15:06:13 +0200

    fixed deprecated use of Double(double)

--- src/main/java/org/diylc/core/measures/Size.java
+++ src/main/java/org/diylc/core/measures/Size.java
@@ -1,23 +1,21 @@
 /*
-
-    DIY Layout Creator (DIYLC).
-    Copyright (c) 2009-2018 held jointly by the individual authors.
-
-    This file is part of DIYLC.
-
-    DIYLC is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    DIYLC is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
-
+  DIY Layout Creator (DIYLC).
+  Copyright (c) 2009-2019 held jointly by the individual authors.
+  
+  This file is part of DIYLC.
+  
+  DIYLC is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  DIYLC is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
 */
 package org.diylc.core.measures;
 
@@ -64,6 +62,8 @@ public class Size extends AbstractMeasure<SizeUnit> implements Comparable<Size> 
 
   @Override
   public int compareTo(Size o) {
-    return new Double(value * unit.getFactor()).compareTo(o.getValue() * o.getUnit().getFactor());
+      return Double.valueOf(value
+			    * unit.getFactor()).compareTo(o.getValue()
+							  * o.getUnit().getFactor());
   }
 }
