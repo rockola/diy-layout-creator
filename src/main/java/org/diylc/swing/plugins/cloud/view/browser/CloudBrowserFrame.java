47ab7567928831bf1d8bd40b844f09fdde32528c
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-01 18:39:03 +0200

    added convenience functions to all ISimpleView implementing classes; this cut'n'paste should be refactored

--- src/main/java/org/diylc/swing/plugins/cloud/view/browser/CloudBrowserFrame.java
+++ src/main/java/org/diylc/swing/plugins/cloud/view/browser/CloudBrowserFrame.java
@@ -42,6 +42,8 @@ import javax.swing.event.ChangeListener;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+
+import org.diylc.common.Config;
 import org.diylc.common.IPlugInPort;
 import org.diylc.common.ITask;
 import org.diylc.core.IView;
