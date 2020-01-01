519ebd8509624bd76cb3c1b0b1440335ff2463f2
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-01 18:24:00 +0200

    indents

--- src/main/java/org/diylc/core/IView.java
+++ src/main/java/org/diylc/core/IView.java
@@ -1,22 +1,21 @@
 /*
+  DIY Layout Creator (DIYLC).
+  Copyright (c) 2009-2020 held jointly by the individual authors.
 
-    DIY Layout Creator (DIYLC).
-    Copyright (c) 2009-2018 held jointly by the individual authors.
+  This file is part of DIYLC.
 
-    This file is part of DIYLC.
+  DIYLC is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    DIYLC is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  DIYLC is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    DIYLC is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 package org.diylc.core;
@@ -35,28 +34,28 @@ import org.diylc.common.PropertyWrapper;
  */
 public interface IView {
 
-  public static final int ERROR_MESSAGE = 0;
-  public static final int INFORMATION_MESSAGE = 1;
-  public static final int WARNING_MESSAGE = 2;
-  public static final int QUESTION_MESSAGE = 3;
-  public static final int PLAIN_MESSAGE = -1;
-  public static final int DEFAULT_OPTION = -1;
-  public static final int YES_NO_OPTION = 0;
-  public static final int YES_NO_CANCEL_OPTION = 1;
-  public static final int OK_CANCEL_OPTION = 2;
-  public static final int YES_OPTION = 0;
-  public static final int NO_OPTION = 1;
-  public static final int CANCEL_OPTION = 2;
-  public static final int OK_OPTION = 0;
+    public static final int ERROR_MESSAGE = 0;
+    public static final int INFORMATION_MESSAGE = 1;
+    public static final int WARNING_MESSAGE = 2;
+    public static final int QUESTION_MESSAGE = 3;
+    public static final int PLAIN_MESSAGE = -1;
+    public static final int DEFAULT_OPTION = -1;
+    public static final int YES_NO_OPTION = 0;
+    public static final int YES_NO_CANCEL_OPTION = 1;
+    public static final int OK_CANCEL_OPTION = 2;
+    public static final int YES_OPTION = 0;
+    public static final int NO_OPTION = 1;
+    public static final int CANCEL_OPTION = 2;
+    public static final int OK_OPTION = 0;
 
-  public static final String CHECK_BOX_MENU_ITEM = "org.diylc.checkBoxMenuItem";
-  public static final String RADIO_BUTTON_GROUP_KEY = "org.diylc.radioButtonGroup";
+    public static final String CHECK_BOX_MENU_ITEM = "org.diylc.checkBoxMenuItem";
+    public static final String RADIO_BUTTON_GROUP_KEY = "org.diylc.radioButtonGroup";
 
-  void showMessage(String message, String title, int messageType);
+    void showMessage(String message, String title, int messageType);
 
-  int showConfirmDialog(String message, String title, int optionType, int messageType);
+    int showConfirmDialog(String message, String title, int optionType, int messageType);
 
-  boolean editProperties(List<PropertyWrapper> properties, Set<PropertyWrapper> defaultedProperties);
+    boolean editProperties(List<PropertyWrapper> properties, Set<PropertyWrapper> defaultedProperties);
 
-  File promptFileSave();
+    File promptFileSave();
 }
