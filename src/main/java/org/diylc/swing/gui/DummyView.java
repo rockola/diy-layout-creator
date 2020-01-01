16377fb708de5edb3f57f523f08d72954295c631
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-01 18:25:23 +0200

    indents

--- src/main/java/org/diylc/swing/gui/DummyView.java
+++ src/main/java/org/diylc/swing/gui/DummyView.java
@@ -1,23 +1,21 @@
 /*
+  DIY Layout Creator (DIYLC).
+  Copyright (c) 2009-2020 held jointly by the individual authors.
 
-    DIY Layout Creator (DIYLC).
-    Copyright (c) 2009-2018 held jointly by the individual authors.
+  This file is part of DIYLC.
 
-    This file is part of DIYLC.
+  DIYLC is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    DIYLC is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  DIYLC is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
 
-    DIYLC is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
-
+  You should have received a copy of the GNU General Public License
+  along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
 */
 package org.diylc.swing.gui;
 
@@ -30,21 +28,21 @@ import org.diylc.core.IView;
 
 public class DummyView implements IView {
 
-  @Override
-  public int showConfirmDialog(String message, String title, int optionType, int messageType) {
-    return 0;
-  }
+    @Override
+    public int showConfirmDialog(String message, String title, int optionType, int messageType) {
+	return 0;
+    }
 
-  @Override
-  public void showMessage(String message, String title, int messageType) {}
+    @Override
+    public void showMessage(String message, String title, int messageType) {}
 
-  @Override
-  public File promptFileSave() {
-    return null;
-  }
+    @Override
+    public File promptFileSave() {
+	return null;
+    }
 
-  @Override
-  public boolean editProperties(List<PropertyWrapper> properties, Set<PropertyWrapper> defaultedProperties) {
-    return false;
-  }
+    @Override
+    public boolean editProperties(List<PropertyWrapper> properties, Set<PropertyWrapper> defaultedProperties) {
+	return false;
+    }
 }
