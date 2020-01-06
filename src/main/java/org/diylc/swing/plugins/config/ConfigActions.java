917f49edde80140b8c993f64481dd5d3293f463d
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-06 22:02:42 +0200

    convenience class for configuring ConfigPlugin

---
+++ src/main/java/org/diylc/swing/plugins/config/ConfigActions.java
@@ -0,0 +1,46 @@
+package org.diylc.swing.plugins.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.diylc.common.IPlugInPort;
+import org.diylc.swing.ActionFactory;
+import org.diylc.swing.ISwingUI;
+
+public class ConfigActions {
+    private List<ConfigAction> actions;
+
+    public ConfigActions() {
+	actions = new ArrayList<ConfigAction>();
+    }
+
+    public void add(String name, String action, boolean defaultValue) {
+	actions.add(new ConfigAction(name, action, defaultValue));
+    }
+
+    public void injectActions(ISwingUI ui, IPlugInPort plugInPort, String menuName) {
+	for (ConfigAction a : actions) {
+	    ui.injectMenuAction(ActionFactory.createConfigAction(plugInPort,
+								 a.getName(),
+								 a.getAction(),
+								 a.getDefault()),
+				menuName);
+	}
+    }
+
+    private class ConfigAction {
+	private String name;
+	private String action;
+	private boolean defaultValue;
+
+	public ConfigAction(String name, String action, boolean defaultValue) {
+	    this.name = name;
+	    this.action = action;
+	    this.defaultValue = defaultValue;
+	}
+
+	public String getName() { return name; }
+	public String getAction() { return action; }
+	public boolean getDefault() { return defaultValue; }
+    }
+}
