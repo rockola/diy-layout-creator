59531f3b3c95a810e62382d4033382653432fbd5
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-07 20:04:58 +0200

    load fonts from resources

---
+++ src/main/java/org/diylc/images/FontLoader.java
@@ -0,0 +1,29 @@
+package org.diylc.images;
+
+import java.awt.Font;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+public final class FontLoader {
+    private FontLoader() { }
+
+    private static Map<String, Font> fonts = new HashMap<String, Font>();
+
+    public static Font getFont(String name, int style, int size) {
+	Font font = fonts.get(name);
+	if (font == null) {
+	    try {
+		InputStream is = FontLoader.class.getResourceAsStream(String.format("/fonts/%s.ttf",
+										    name));
+		font = Font.createFont(Font.TRUETYPE_FONT, is);
+		fonts.put(name, font);
+	    } catch (Exception ex) {
+		font = new Font("serif", style, size);
+	    }
+	}
+	return font.deriveFont(style, size);
+    }
+}
