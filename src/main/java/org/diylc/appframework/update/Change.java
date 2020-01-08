d52c0c67d00d9c645eb3df2c0877f3fd5c716ae7
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-08 15:45:09 +0200

    indents

--- src/main/java/org/diylc/appframework/update/Change.java
+++ src/main/java/org/diylc/appframework/update/Change.java
@@ -1,69 +1,93 @@
+/*
+  DIY Layout Creator (DIYLC).
+  Copyright (c) 2009-2020 held jointly by the individual authors.
+
+  This file is part of DIYLC.
+
+  DIYLC is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DIYLC is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+  License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with DIYLC. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.diylc.appframework.update;
 
 import java.io.Serializable;
 
 public class Change implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private ChangeType changeType;
-	private String description;
+    private ChangeType changeType;
+    private String description;
 
-	public Change(ChangeType changeType, String description) {
-		super();
-		this.changeType = changeType;
-		this.description = description;
-	}
+    public Change(ChangeType changeType, String description) {
+	super();
+	this.changeType = changeType;
+	this.description = description;
+    }
 
-	public ChangeType getChangeType() {
-		return changeType;
-	}
+    public Change(ChangeType changeType) {
+	super();
+	this.changeType = changeType;
+    }
 
-	public void setChangeType(ChangeType changeType) {
-		this.changeType = changeType;
-	}
+    public ChangeType getChangeType() {
+	return changeType;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setChangeType(ChangeType changeType) {
+	this.changeType = changeType;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public String getDescription() {
+	return description;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((changeType == null) ? 0 : changeType.hashCode());
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		return result;
-	}
+    public void setDescription(String description) {
+	this.description = description;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Change other = (Change) obj;
-		if (changeType == null) {
-			if (other.changeType != null)
-				return false;
-		} else if (!changeType.equals(other.changeType))
-			return false;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((changeType == null) ? 0 : changeType.hashCode());
+	result = prime * result + ((description == null) ? 0 : description.hashCode());
+	return result;
+    }
 
-	@Override
-	public String toString() {
-		return "[" + changeType + "] " + description;
-	}
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Change other = (Change) obj;
+	if (changeType == null) {
+	    if (other.changeType != null)
+		return false;
+	} else if (!changeType.equals(other.changeType))
+	    return false;
+	if (description == null) {
+	    if (other.description != null)
+		return false;
+	} else if (!description.equals(other.description))
+	    return false;
+	return true;
+    }
+
+    @Override
+    public String toString() {
+	return "[" + changeType + "] " + description;
+    }
 }
