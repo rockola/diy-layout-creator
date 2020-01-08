1b347b9364f8ad3377b7bf082fbfb4434416145b
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-08 15:45:41 +0200

    parse changeType from String

--- src/main/java/org/diylc/appframework/update/ChangeType.java
+++ src/main/java/org/diylc/appframework/update/ChangeType.java
@@ -1,17 +1,53 @@
+/*
+  DIY Layout Creator (DIYLC).
+  Copyright (c) 2009-2020 held jointly by the individual authors.
+
+  This file is part of DIYLC.
+
+  DIYLC is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DIYLC is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+  License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with DIYLC. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.diylc.appframework.update;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public enum ChangeType {
 
-	BUG_FIX("Bug Fix"), NEW_FEATURE("New Feature"), IMPROVEMENT("Improvement");
+    BUG_FIX("Bug Fix"),
+    NEW_FEATURE("New Feature"),
+    IMPROVEMENT("Improvement");
 
-	String name;
+    private String name;
 
-	private ChangeType(String name) {
-		this.name = name;
+    private ChangeType(String name) {
+	this.name = name;
+    }
+
+    @Override
+    public String toString() {
+	return name;
+    }
+
+    public static ChangeType parseName(String name) {
+	switch (name) {
+	case "Bug Fix": return BUG_FIX;
+	case "New Feature": return NEW_FEATURE;
+	case "Improvement": return IMPROVEMENT;
+	default:
+	    LogManager.getLogger(ChangeType.class).error("parseName({}) not a valid ChangeType",
+							 name);
+	    return null;
 	}
-
-	@Override
-	public String toString() {
-		return name;
-	}
+    }
 }
