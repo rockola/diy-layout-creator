10333b7a7f61478d657d42065839a504bf240180
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-08 10:31:32 +0200

    create icons from intermediate PNG while ImageMagick does not know how to scale SVG-to-PNG conversion

--- src/original-icons/diylc/create-png.sh
+++ src/original-icons/diylc/create-png.sh
@@ -4,11 +4,17 @@
 
 SRC=diylc.svg
 TARGETDIR=../../../target/PNG
+INTERMEDIATE=${TARGETDIR}/diylc.png
 
 mkdir -p ${TARGETDIR}
+# First, create full size PNG
+# because ImageMagick does not know how to create different
+# sized PNG files straight from SVG...
+# Supposedly IM7 will though, hope so
+convert ${SRC} -transparent white ${INTERMEDIATE}
 for DIM in 16 32 48 64 96 128 256 512 1024
 do
     TARGET=`basename $SRC .svg`-${DIM}x${DIM}.png
     echo ${TARGET}
-    convert -size ${DIM}x${DIM} ${SRC} -transparent white ${TARGETDIR}/${TARGET}
+    convert ${INTERMEDIATE} -geometry ${DIM}x${DIM}  -transparent white ${TARGETDIR}/${TARGET}
 done
