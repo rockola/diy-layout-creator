d822850fa95c66eb363dd2491468a339e6718b33
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-13 18:06:40 +0200

    typofix

--- src/main/java/org/diylc/presenter/PCBLayerFilter.java
+++ src/main/java/org/diylc/presenter/PCBLayerFilter.java
@@ -4,32 +4,31 @@
 
   This file is part of DIYLC.
 
-  DIYLC is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
+  DIYLC is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  DIYLC is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  DIYLC is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
 
   You should have received a copy of the GNU General Public License
   along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
-
 */
 package org.diylc.presenter;
 
 import java.lang.reflect.Method;
-import org.diylc.common.IComponentFiler;
+import org.diylc.common.IComponentFilter;
 import org.diylc.common.PCBLayer;
 import org.diylc.core.IDIYComponent;
 
-public class PCBLayerFiler implements IComponentFiler {
+public class PCBLayerFilter implements IComponentFilter {
 
   private PCBLayer layer;
 
-  public PCBLayerFiler(PCBLayer layer) {
+  public PCBLayerFilter(PCBLayer layer) {
     super();
     this.layer = layer;
   }
