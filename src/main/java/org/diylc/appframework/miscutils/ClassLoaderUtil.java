9c4d23924eabd6aa96f1cefb033a10fb82314e1e
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-14 13:02:20 +0200

    typos

--- src/main/java/org/diylc/appframework/miscutils/ClassLoaderUtil.java
+++ src/main/java/org/diylc/appframework/miscutils/ClassLoaderUtil.java
@@ -1,3 +1,23 @@
+/*
+  DIY Layout Creator (DIYLC).
+  Copyright (c) 2009-2020 held jointly by the individual authors.
+
+  This file is part of DIYLC.
+
+  DIYLC is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DIYLC is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package org.diylc.appframework.miscutils;
 
 import java.io.File;
@@ -5,18 +25,17 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ClassLoaderUtil {
-  // Log object
-  private static Logger LOG = LogManager.getLogger(ClassLoaderUtil.class);
 
-  // Parameters
+  private static Logger LOG = LogManager.getLogger(ClassLoaderUtil.class);
   private static final Class<?>[] parameters = new Class<?>[] {URL.class};
 
   /**
-   * Add file to CLASSPATH
+   * Add file to CLASSPATH.
    *
    * @param s File name
    * @throws IOException IOException
@@ -27,7 +46,7 @@ public class ClassLoaderUtil {
   }
 
   /**
-   * Add file to CLASSPATH
+   * Add file to CLASSPATH.
    *
    * @param f File object
    * @throws IOException IOException
@@ -38,7 +57,7 @@ public class ClassLoaderUtil {
   }
 
   /**
-   * Add URL to CLASSPATH
+   * Add URL to CLASSPATH.
    *
    * @param u URL
    * @throws IOException IOException
