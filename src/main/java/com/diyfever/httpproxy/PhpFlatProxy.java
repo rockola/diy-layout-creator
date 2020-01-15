58a3cb4b44ddfde7ac9ce44bc5b5041294bfaef1
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-15 17:12:38 +0200

    started work to convert to OkHttp, commented out for now

--- src/main/java/com/diyfever/httpproxy/PhpFlatProxy.java
+++ src/main/java/com/diyfever/httpproxy/PhpFlatProxy.java
@@ -7,6 +7,14 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.MultipartBody;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.diylc.appframework.Serializer;
@@ -26,28 +34,50 @@ import org.diylc.appframework.Serializer;
 public class PhpFlatProxy implements IFlatProxy {
 
   private static final Logger LOG = LogManager.getLogger(PhpFlatProxy.class);
+  private final OkHttpClient httpClient = new OkHttpClient();
 
   @Override
   public InputStream invoke(String url, String methodName, Map<String, Object> params) {
     InputStream serverInput;
+    URL phpUrl = new java.net.URL(createPhpFileName(url, methodName));
     try {
+      // Call the server.
+      /*
+      MultipartBody.Builder builder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+      for (Map.Entry<String, Object> entry : params.entrySet()) {
+        if (entry.getValue() != null) {
+          final String key = entry.getKey();
+          //if (entry.getValue is not a file
+          if (entry.getValue() instanceof File) {
+            File file = entry.getValue();
+            builder = builder.addFormDataPart(
+                key,
+                file.getPath(),
+                RequestBody.create(XXX, file));
+          } else {
+            // not a File
+            final String value = entry.getValue().toString();
+            LOG.trace("invoke({}, {}, params) adding {}={}", url, methodName, key, value);
+            builder = builder.addFormDataPart(key, value);
+          }
+        }
+      }
+      RequestBody body = builder.build();
+      LOG.debug("Connecting to {}", phpUrl);
+      Request request = new Request.Builder().url(phpUrl).post(body).build();
+      try (Response response = httpClient.newCall(request).execute()) {
+        return response.body();
+        }*/
       // Flatten params map into an array
       List<Object> paramList = new ArrayList<Object>();
-
       for (Map.Entry<String, Object> entry : params.entrySet()) {
         if (entry.getValue() != null) {
           paramList.add(entry.getKey());
           paramList.add(entry.getValue());
         }
       }
-
-      // Call the server.
-      URL phpUrl = new java.net.URL(createPhpFileName(url, methodName));
-      LOG.debug("Connecting to: " + phpUrl);
       serverInput = ClientHttpRequest.post(phpUrl, paramList.toArray());
       return serverInput;
-    } catch (MalformedURLException e) {
-      LOG.error(e);
     } catch (IOException e) {
       LOG.error(e);
     }
@@ -62,7 +92,9 @@ public class PhpFlatProxy implements IFlatProxy {
     }
     // Deserialize the stream
     try {
-      return Serializer.fromInputStream(stream);
+      Object o = Serializer.fromInputStream(stream);
+      stream.close();
+      return o;
     } catch (IOException e) {
       LOG.error(e);
     }
