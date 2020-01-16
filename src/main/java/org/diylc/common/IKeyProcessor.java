14d94fbbd4be5e9813bffbfb291bed9bcf6cbb8c
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-16 07:57:30 +0200

    removed unnecessary modifiers from interface

--- src/main/java/org/diylc/common/IKeyProcessor.java
+++ src/main/java/org/diylc/common/IKeyProcessor.java
@@ -22,12 +22,12 @@ package org.diylc.common;
 
 public interface IKeyProcessor {
 
-  public static final int VK_LEFT = 0x25;
-  public static final int VK_UP = 0x26;
-  public static final int VK_RIGHT = 0x27;
-  public static final int VK_DOWN = 0x28;
-  public static final int VK_H = 0x48;
-  public static final int VK_V = 0x56;
+  int VK_LEFT = 0x25;
+  int VK_UP = 0x26;
+  int VK_RIGHT = 0x27;
+  int VK_DOWN = 0x28;
+  int VK_H = 0x48;
+  int VK_V = 0x56;
 
   /**
    * Notifies the presenter that a key hes been pressed on the canvas.
