b3fcd7a6d886eea3d07f8e1791b468455b7dda0c
Ola Rinta-Koski <github@rinta-koski.net>
2020-01-16 07:57:07 +0200

    removed unnecessary modifiers from interface

--- src/main/java/org/diylc/common/IComponentTransformer.java
+++ src/main/java/org/diylc/common/IComponentTransformer.java
@@ -25,11 +25,11 @@ import org.diylc.core.IDIYComponent;
 
 public interface IComponentTransformer {
 
-  public static final int CLOCKWISE = 1;
-  public static final int COUNTER_CLOCKWISE = -1;
+  int CLOCKWISE = 1;
+  int COUNTER_CLOCKWISE = -1;
 
-  public static final int HORIZONTAL = 0;
-  public static final int VERTICAL = 1;
+  int HORIZONTAL = 0;
+  int VERTICAL = 1;
 
   boolean canRotate(IDIYComponent<?> component);
 
