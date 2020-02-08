ca5a1633c48b7b40d104bc73a5eebf3fef137c20
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-08 17:06:55 +0200

    superclass for radial capacitors - todo: combine more of the common capacitor functionality into a superclass

---
+++ src/main/java/org/diylc/components/passive/RadialCapacitor.java
@@ -0,0 +1,84 @@
+/*
+  DIY Layout Creator (DIYLC).
+  Copyright (c) 2009-2018 held jointly by the individual authors.
+
+  This file is part of DIYLC.
+
+  DIYLC is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DIYLC is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.diylc.components.passive;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.RoundRectangle2D;
+
+import org.diylc.common.SimpleComponentTransformer;
+import org.diylc.components.AbstractRadialComponent;
+import org.diylc.core.CreationMethod;
+import org.diylc.core.IDIYComponent;
+import org.diylc.core.annotations.ComponentDescriptor;
+import org.diylc.core.annotations.EditableProperty;
+import org.diylc.core.annotations.PositiveMeasureValidator;
+import org.diylc.core.measures.Capacitance;
+import org.diylc.core.measures.Size;
+import org.diylc.core.measures.SizeUnit;
+
+public abstract class RadialCapacitor extends AbstractRadialComponent<Capacitance> {
+
+  private static final long serialVersionUID = 1L;
+
+  protected Capacitance value = null;
+  @Deprecated protected Voltage voltage = Voltage._63V;
+  protected org.diylc.core.measures.Voltage voltageNew = null;
+
+  protected RadialCapacitor() {
+    super();
+  }
+
+  @EditableProperty(validatorClass = PositiveMeasureValidator.class)
+  public Capacitance getValue() {
+    return value;
+  }
+
+  public void setValue(Capacitance value) {
+    this.value = value;
+  }
+
+  @Override
+  public String getValueForDisplay() {
+    return getValue().toString()
+        + (getVoltageNew() == null ? "" : " " + getVoltageNew().toString());
+  }
+
+  @Deprecated
+  public Voltage getVoltage() {
+    return voltage;
+  }
+
+  @Deprecated
+  public void setVoltage(Voltage voltage) {
+    this.voltage = voltage;
+  }
+
+  @EditableProperty(name = "Voltage")
+  public org.diylc.core.measures.Voltage getVoltageNew() {
+    return voltageNew;
+  }
+
+  public void setVoltageNew(org.diylc.core.measures.Voltage voltageNew) {
+    this.voltageNew = voltageNew;
+  }
+}
