b1a13703276133bd75c0c11ffa4b5130abb1ed29
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-08 17:47:53 +0200

    changes for new extractFrom interface

--- src/main/java/org/diylc/utils/KeywordExtractor.java
+++ src/main/java/org/diylc/utils/KeywordExtractor.java
@@ -1,28 +1,29 @@
 /*
+  DIY Layout Creator (DIYLC).
+  Copyright (c) 2009-2018 held jointly by the individual authors.
 
-    DIY Layout Creator (DIYLC).
-    Copyright (c) 2009-2018 held jointly by the individual authors.
+  This file is part of DIYLC.
 
-    This file is part of DIYLC.
+  DIYLC is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    DIYLC is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  DIYLC is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
 
-    DIYLC is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
-
+  You should have received a copy of the GNU General Public License
+  along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 package org.diylc.utils;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.StringJoiner;
+
 import org.diylc.common.ComponentType;
 import org.diylc.core.IDIYComponent;
 import org.diylc.core.Project;
@@ -42,29 +43,30 @@ public class KeywordExtractor {
 
   private KeywordExtractor() {}
 
-  @SuppressWarnings("unchecked")
   public String extractKeywords(Project project) {
     Set<String> words = new HashSet<String>();
     for (IDIYComponent<?> c : project.getComponents()) {
-      ComponentType cType =
-          ComponentProcessor.extractComponentTypeFrom(
-              (Class<? extends IDIYComponent<?>>) c.getClass());
-      if (cType.getKeywordPolicy() == KeywordPolicy.SHOW_TYPE_NAME)
-        words.add(cType.getName().toLowerCase());
-      if ((cType.getKeywordPolicy() == KeywordPolicy.SHOW_VALUE
-              || cType.getKeywordPolicy() == KeywordPolicy.SHOW_TAG_AND_VALUE)
-          && c.getValueForDisplay() != null
-          && c.getValueForDisplay().trim().length() > 0)
-        words.add(c.getValueForDisplay().trim().toLowerCase());
-      if ((cType.getKeywordPolicy() == KeywordPolicy.SHOW_TAG
-           || cType.getKeywordPolicy() == KeywordPolicy.SHOW_TAG_AND_VALUE)
-          && cType.getKeywordTag() != null
-          && cType.getKeywordTag().length() > 0)
-        words.add(cType.getKeywordTag().trim().toLowerCase());
+      ComponentType componentType = ComponentType.extractFrom(c);
+      KeywordPolicy policy = componentType.getKeywordPolicy();
+      if (policy == KeywordPolicy.SHOW_TYPE_NAME) {
+        words.add(componentType.getName().toLowerCase());
+      } else {
+        if (policy.showValue()
+            && c.getValueForDisplay() != null
+            && c.getValueForDisplay().trim().length() > 0) {
+          words.add(c.getValueForDisplay().trim().toLowerCase());
+        }
+        if (policy.showTag()
+            && componentType.getKeywordTag() != null
+            && componentType.getKeywordTag().length() > 0) {
+          words.add(componentType.getKeywordTag().trim().toLowerCase());
+        }
+      }
     }
-    StringBuilder sb = new StringBuilder();
-    for (String w : words) sb.append(w).append(",");
-    if (sb.length() > 0) sb.deleteCharAt(sb.length() - 1);
-    return sb.toString();
+    StringJoiner sj = new StringJoiner(",");
+    for (String word : words) {
+      sj.add(word);
+    }
+    return sj.toString();
   }
 }
