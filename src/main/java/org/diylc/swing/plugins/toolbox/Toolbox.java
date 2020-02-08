3e573a38af24492e54c87270205b48bf544614ab
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-08 18:06:00 +0200

    style fixes

--- src/main/java/org/diylc/swing/plugins/toolbox/Toolbox.java
+++ src/main/java/org/diylc/swing/plugins/toolbox/Toolbox.java
@@ -21,7 +21,6 @@
 package org.diylc.swing.plugins.toolbox;
 
 import java.util.EnumSet;
-import javax.swing.SwingConstants;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -29,53 +28,25 @@ import org.apache.logging.log4j.Logger;
 import org.diylc.App;
 import org.diylc.appframework.miscutils.ConfigurationManager;
 import org.diylc.appframework.miscutils.IConfigListener;
-import org.diylc.common.BadPositionException;
 import org.diylc.common.EventType;
 import org.diylc.common.IPlugIn;
 import org.diylc.common.IPlugInPort;
 import org.diylc.swing.plugins.config.ConfigPlugin;
-import org.diylc.swing.plugins.statusbar.StatusBar;
 
-public class ToolBox implements IPlugIn {
+public class Toolbox implements IPlugIn {
 
-  private static final Logger LOG = LogManager.getLogger(StatusBar.class);
+  private static final Logger LOG = LogManager.getLogger(Toolbox.class);
 
-  private IPlugInPort plugInPort;
-  private ComponentTabbedPane componentTabbedPane;
-
-  public ToolBox() {}
+  public Toolbox() {}
 
   @Override
-  public void connect(IPlugInPort plugInPort) {
-    this.plugInPort = plugInPort;
-    try {
-      App.ui().injectGUIComponent(getComponentTabbedPane(), SwingConstants.TOP);
-    } catch (BadPositionException e) {
-      LOG.error("Could not install the toolbox", e);
-    }
+  public void connect(IPlugInPort ignore) {
+    LOG.trace("connect() adding component browser listener");
     ConfigurationManager.addListener(
         ConfigPlugin.COMPONENT_BROWSER,
-        new IConfigListener() {
-
-          @Override
-          public void valueChanged(String key, Object value) {
-            getComponentTabbedPane()
-                .setVisible(
-                    ConfigPlugin.COMPONENT_BROWSER.equals(key)
-                        && ConfigPlugin.TABBED_TOOLBAR.equals(value));
-          }
-        });
-
-    getComponentTabbedPane().setVisible(App.getString(
-        ConfigPlugin.COMPONENT_BROWSER,
-        ConfigPlugin.SEARCHABLE_TREE).equals(ConfigPlugin.TABBED_TOOLBAR));
-  }
-
-  public ComponentTabbedPane getComponentTabbedPane() {
-    if (componentTabbedPane == null) {
-      componentTabbedPane = new ComponentTabbedPane(plugInPort);
-    }
-    return componentTabbedPane;
+        (key, value) -> App.ui().getComponentTabbedPane().setVisible(
+            ConfigPlugin.COMPONENT_BROWSER.equals(key)
+            && ConfigPlugin.TABBED_TOOLBAR.equals(value)));
   }
 
   @Override
