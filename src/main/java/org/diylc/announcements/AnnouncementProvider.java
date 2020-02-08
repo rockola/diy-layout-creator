760b533b9e59be2d32136a7254728f2f9cb12b35
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-08 17:13:50 +0200

    stylecheck fixes

--- src/main/java/org/diylc/announcements/AnnouncementProvider.java
+++ src/main/java/org/diylc/announcements/AnnouncementProvider.java
@@ -41,7 +41,7 @@ import org.diylc.App;
 
 public class AnnouncementProvider {
 
-  private Logger LOG = LogManager.getLogger(AnnouncementProvider.class);
+  private static final Logger LOG = LogManager.getLogger(AnnouncementProvider.class);
 
   private String serviceUrl = App.getURL("api.announcements").toString();
   private final String lastReadKey = "announcement.lastReadDate";
@@ -56,31 +56,37 @@ public class AnnouncementProvider {
     renderer = HtmlRenderer.builder().build();
     String lastDateStr = App.getString(lastReadKey);
     try {
-      this.lastDate = (lastDateStr == null ? null : dateFormat.parse(lastDateStr));
+      lastDate = lastDateStr == null
+                 ? null
+                 : dateFormat.parse(lastDateStr);
+      LOG.info("Announcement last read {}", lastDate);
     } catch (ParseException e) {
-      // nothing here, why is this not a problem? //ola 20200107
+      LOG.error("Could not parse last read date {}", lastDateStr);
     }
   }
 
   public String getCurrentAnnouncements(boolean forceLast) throws IOException {
     Request request = new Request.Builder().url(serviceUrl).build();
+    String announcements = null;
     try (Response response = httpClient.newCall(request).execute()) {
+      if (response != null && response.body() != null) {
+        Node document = parser.parse(response.body().string());
+        // render replacing newlines with spaces
+        // (JOptionPane wants the HTML in one line...)
+        //
+        // Can't use HtmlRenderer.builder().softbreak(" ") here
+        // as that will not get rid of newlines between tags
+        String txt = renderer.render(document).replace("\n", " ");
 
-      Node document = parser.parse(response.body().string());
-      // render replacing newlines with spaces
-      // (JOptionPane wants the HTML in one line...)
-      //
-      // Can't use HtmlRenderer.builder().softbreak(" ") here
-      // as that will not get rid of newlines between tags
-      String txt = renderer.render(document).replace("\n", " ");
+        // TODO: only show announcements newer than lastReadKey
 
-      // TODO: only show announcements newer than lastReadKey
+        LOG.debug("Announcements [{}]", txt);
 
-      LOG.debug("Announcements [{}]", txt);
-
-      // wrap in HTML tags for JOptionPane
-      return String.format("<html>%s</html>", txt);
+        // wrap in HTML tags for JOptionPane
+        announcements = String.format("<html>%s</html>", txt);
+      }
     }
+    return announcements;
   }
 
   public void dismissed() {
