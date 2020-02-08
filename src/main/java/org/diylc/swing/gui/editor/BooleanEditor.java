3979a659209247779456fdd2e765eaf4164fb7cd
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-08 18:44:35 +0200

    style fixes

--- src/main/java/org/diylc/swing/gui/editor/BooleanEditor.java
+++ src/main/java/org/diylc/swing/gui/editor/BooleanEditor.java
@@ -37,16 +37,11 @@ public class BooleanEditor extends JCheckBox {
   public BooleanEditor(final PropertyWrapper property) {
     super();
     setSelected((Boolean) property.getValue());
-    addActionListener(
-        new ActionListener() {
-
-          @Override
-          public void actionPerformed(ActionEvent e) {
-            property.setChanged(true);
-            setBackground(oldBg);
-            property.setValue(isSelected());
-          }
-        });
+    addActionListener((e) -> {
+        property.setChanged(true);
+        setBackground(oldBg);
+        property.setValue(isSelected());
+      });
     if (!property.isUnique()) {
       setBackground(Constants.MULTI_VALUE_COLOR);
     }
