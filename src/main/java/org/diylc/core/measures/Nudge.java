c8c9688005d034693d567b038fac1980193a8d13
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-09 20:37:16 +0200

    style & syntactic sugar

--- src/main/java/org/diylc/core/measures/Nudge.java
+++ src/main/java/org/diylc/core/measures/Nudge.java
@@ -1,50 +1,58 @@
 /*
+  DIY Layout Creator (DIYLC).
+  Copyright (c) 2009-2018 held jointly by the individual authors.
 
-    DIY Layout Creator (DIYLC).
-    Copyright (c) 2009-2018 held jointly by the individual authors.
+  This file is part of DIYLC.
 
-    This file is part of DIYLC.
+  DIYLC is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    DIYLC is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  DIYLC is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
 
-    DIYLC is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
-
+  You should have received a copy of the GNU General Public License
+  along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 package org.diylc.core.measures;
 
 import org.diylc.core.annotations.EditableProperty;
 
 public class Nudge {
 
-  private Size xOffset;
-  private Size yOffset;
+  private Size offsetX;
+  private Size offsetY;
   private boolean affectStuckComponents;
 
   @EditableProperty(name = "X-axis", sortOrder = 1)
-  public Size getxOffset() {
-    return xOffset;
+  public Size getOffsetX() {
+    return offsetX;
   }
 
-  public void setxOffset(Size xOffset) {
-    this.xOffset = xOffset;
+  public void setOffsetX(Size offsetX) {
+    this.offsetX = offsetX;
   }
 
   @EditableProperty(name = "Y-axis", sortOrder = 2)
-  public Size getyOffset() {
-    return yOffset;
+  public Size getOffsetY() {
+    return offsetY;
   }
 
-  public void setyOffset(Size yOffset) {
-    this.yOffset = yOffset;
+  public void setOffsetY(Size offsetY) {
+    this.offsetY = offsetY;
+  }
+
+  public void setOffsets(Size offsetX, Size offsetY) {
+    setOffsetX(offsetX);
+    setOffsetY(offsetY);
+  }
+
+  public void setOffsets(Size offset) {
+    setOffsets(offset, offset);
   }
 
   @EditableProperty(name = "Include stuck", sortOrder = 10)
