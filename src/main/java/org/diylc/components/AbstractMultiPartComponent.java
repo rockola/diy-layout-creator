ef5aa5d5af06de264ad2838325185cccd7664e7d
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-09 15:34:39 +0200

    style, debug

--- src/main/java/org/diylc/components/AbstractMultiPartComponent.java
+++ src/main/java/org/diylc/components/AbstractMultiPartComponent.java
@@ -1,8 +1,12 @@
 package org.diylc.components;
 
 import java.awt.Graphics2D;
-import java.awt.geom.Area;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.diylc.common.ObjectCache;
+import org.diylc.components.Area;
 import org.diylc.core.ComponentState;
 import org.diylc.core.IDrawingObserver;
 import org.diylc.core.Project;
@@ -10,6 +14,7 @@ import org.diylc.core.Project;
 public abstract class AbstractMultiPartComponent<T> extends AbstractTransparentComponent<T> {
 
   private static final long serialVersionUID = 1L;
+  private static final Logger LOG = LogManager.getLogger(AbstractMultiPartComponent.class);
 
   public abstract Area[] getBody();
 
@@ -30,6 +35,7 @@ public abstract class AbstractMultiPartComponent<T> extends AbstractTransparentC
     Area[] body = getBody();
     Area outline = new Area();
     for (Area b : body) {
+      LOG.trace("Body size {}: {} area", body.length, b == null ? "Not drawing" : "Drawing");
       if (b != null) {
         outline.add(b);
       }
