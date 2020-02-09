4221e7a18944799aba3f0302fea86ec552417b8b
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-09 20:46:30 +0200

    read themes with XmlReader

--- src/main/java/org/diylc/core/Theme.java
+++ src/main/java/org/diylc/core/Theme.java
@@ -1,37 +1,91 @@
 /*
+  DIY Layout Creator (DIYLC).
+  Copyright (c) 2009-2018 held jointly by the individual authors.
 
-    DIY Layout Creator (DIYLC).
-    Copyright (c) 2009-2018 held jointly by the individual authors.
+  This file is part of DIYLC.
 
-    This file is part of DIYLC.
+  DIYLC is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    DIYLC is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  DIYLC is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    DIYLC is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
-
+  You should have received a copy of the GNU General Public License
+  along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 package org.diylc.core;
 
 import java.awt.Color;
+import java.io.File;
 import java.io.Serializable;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.beryx.awt.color.ColorFactory;
+
+import org.diylc.App;
+import org.diylc.parsing.XmlNode;
+import org.diylc.parsing.XmlReader;
+import org.diylc.appframework.miscutils.Utils;
 
 public class Theme implements Serializable {
 
   private static final long serialVersionUID = 1L;
+  private static Logger LOG = LogManager.getLogger(Theme.class);
+
+  public static final String THEME_KEY = "theme";
+  public static final Theme DEFAULT_THEME = new Theme(
+      "Default", Color.white, new Color(240, 240, 240), Color.black);
+
+  private static Map<String, Theme> themes = new HashMap<>();
 
   private String name;
   private Color bgColor;
   private Color gridColor;
   private Color outlineColor;
+
+  static {
+    themes.put("Default", DEFAULT_THEME);
+
+    URL themesUrl = Theme.class.getResource("/themes/themes.xml");
+    XmlNode themesXml = XmlReader.read(themesUrl);
+    themesXml.children.asMap().entrySet().stream().forEach((e) -> {
+        String tag = e.getKey();
+        if (tag.toLowerCase().equals("theme")) {
+          for (XmlNode node : e.getValue()) {
+            Theme theme = new Theme(node);
+            LOG.trace("Found theme {}", theme.getName());
+            themes.put(theme.getName(), theme);
+          }
+        } else {
+          LOG.error("unknown element {} in themes.xml", tag);
+        }
+      });
+
+    File themeDir = new File(Utils.getUserDataDirectory("themes"));
+    if (themeDir.exists()) {
+      for (File file : themeDir.listFiles()) {
+        if (file.getName().toLowerCase().endsWith(".xml")) {
+          try {
+            Theme theme = new Theme(XmlReader.read(file));
+            LOG.debug("Found theme {}", theme.getName());
+            Theme.getThemes().put(theme.getName(), theme);
+          } catch (Exception e) {
+            LOG.info("Could not load theme file " + file.getName(), e);
+          }
+        }
+      }
+    }
+  }
 
   public Theme(String name, Color bgColor, Color gridColor, Color outlineColor) {
     super();
@@ -39,6 +93,74 @@ public class Theme implements Serializable {
     this.bgColor = bgColor;
     this.gridColor = gridColor;
     this.outlineColor = outlineColor;
+  }
+
+  public Theme(Theme theme) {
+    super();
+    this.name = theme.name;
+    this.bgColor = theme.bgColor;
+    this.bgColor = theme.bgColor;
+    this.outlineColor = theme.outlineColor;
+  }
+
+  public Theme() {
+    this(DEFAULT_THEME);
+  }
+
+  public Theme(XmlNode xml) {
+    this();
+    this.name = xml.attributes.get("name");
+    xml.children.entries().stream().forEach((e) -> {
+        XmlNode node = e.getValue();
+        switch (node.tagName) {
+          case "background-color":
+            this.bgColor = getColor(node);
+            break;
+          case "grid-color":
+            this.gridColor = getColor(node);
+            break;
+          case "outline-color":
+            this.outlineColor = getColor(node);
+            break;
+          default:
+        }
+      });
+  }
+
+  private Color getColor(XmlNode xml) {
+    // TODO: opacity <1
+    int red = 0;
+    int green = 0;
+    int blue = 0;
+    for (String key : xml.attributes.keySet()) {
+      String value = xml.attributes.get(key);
+      switch (key) {
+        case "ref":
+          return ColorFactory.valueOf(value);
+        case "grayscale":
+          int v = Integer.parseInt(value);
+          return new Color(v, v, v);
+        case "r":
+          red = Integer.parseInt(value);
+          break;
+        case "g":
+          green = Integer.parseInt(value);
+          break;
+        case "b":
+          blue = Integer.parseInt(value);
+          break;
+        default:
+      }
+    }
+    return new Color(red, green, blue);
+  }
+
+  public static Map<String, Theme> getThemes() {
+    return themes;
+  }
+
+  public static Theme getTheme(String themeName) {
+    return themes.get(themeName);
   }
 
   public String getName() {
