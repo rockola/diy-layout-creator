ab31b77a93ca96bb2d432420097364edcc51739c
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-09 20:01:52 +0200

    funcs for querying point in outline/continuity area

--- src/main/java/org/diylc/presenter/ComponentArea.java
+++ src/main/java/org/diylc/presenter/ComponentArea.java
@@ -17,11 +17,14 @@
   You should have received a copy of the GNU General Public License
   along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 package org.diylc.presenter;
 
 import java.awt.Rectangle;
-import java.awt.geom.Area;
+import java.awt.geom.Point2D;
 import java.util.List;
+
+import org.diylc.components.Area;
 
 public class ComponentArea {
 
@@ -39,31 +42,54 @@ public class ComponentArea {
     this.continuityNegativeAreas = continuityNegativeAreas;
   }
 
+  // TODO: use a standard XML tag library function
+  private String tag(String tagName, String contents) {
+    return contents == null
+        ? String.format("<%s/>", tagName)
+        : String.format("<%s>%s</%s>", tagName, contents, tagName);
+  }
+
   public String toString() {
     if (outlineArea == null) {
-      return "<ComponentArea/>";
+      return tag("ComponentArea", null);
     }
     if (outlineArea.isEmpty()) {
-      return "<ComponentArea><outlineArea/></ComponentArea>";
+      return tag("ComponentArea", tag("outlineArea", null));
     }
     Rectangle bounds = outlineArea.getBounds();
-    return String.format(
-        "<ComponentArea><outlineArea><bounds h=\"%d\" w=\"%d\" x=\"%d\" y=\"%d\"/></outlineArea></ComponentArea>",
+    return tag("componentArea", tag("outlineArea", String.format(
+        "<bounds h=\"%d\" w=\"%d\" x=\"%d\" y=\"%d\"/>",
         bounds.height,
         bounds.width,
         bounds.x,
-        bounds.y);
+        bounds.y)));
   }
 
   public Area getOutlineArea() {
     return outlineArea;
   }
 
+  public boolean inOutlineArea(Point2D point) {
+    return outlineArea != null && point != null && outlineArea.contains(point);
+  }
+
+  public boolean intersectsOutlineArea(Rectangle rect) {
+    return outlineArea != null && rect != null && outlineArea.intersects(rect);
+  }
+
   public List<Area> getContinuityPositiveAreas() {
     return continuityPositiveAreas;
   }
 
+  public boolean hasContinuityPositiveAreas() {
+    return continuityPositiveAreas != null && !continuityPositiveAreas.isEmpty();
+  }
+
   public List<Area> getContinuityNegativeAreas() {
     return continuityNegativeAreas;
+  }
+
+  public boolean hasContinuityNegativeAreas() {
+    return continuityNegativeAreas != null && !continuityNegativeAreas.isEmpty();
   }
 }
