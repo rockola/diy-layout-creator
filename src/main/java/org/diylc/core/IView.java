77088efe043bc5e12ee1bb7f45b02fce097b02b6
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-09 20:45:50 +0200

    style

--- src/main/java/org/diylc/core/IView.java
+++ src/main/java/org/diylc/core/IView.java
@@ -32,22 +32,22 @@ import org.diylc.common.PropertyWrapper;
  */
 public interface IView {
 
-  public static final int ERROR_MESSAGE = 0;
-  public static final int INFORMATION_MESSAGE = 1;
-  public static final int WARNING_MESSAGE = 2;
-  public static final int QUESTION_MESSAGE = 3;
-  public static final int PLAIN_MESSAGE = -1;
-  public static final int DEFAULT_OPTION = -1;
-  public static final int YES_NO_OPTION = 0;
-  public static final int YES_NO_CANCEL_OPTION = 1;
-  public static final int OK_CANCEL_OPTION = 2;
-  public static final int YES_OPTION = 0;
-  public static final int NO_OPTION = 1;
-  public static final int CANCEL_OPTION = 2;
-  public static final int OK_OPTION = 0;
+  int ERROR_MESSAGE = 0;
+  int INFORMATION_MESSAGE = 1;
+  int WARNING_MESSAGE = 2;
+  int QUESTION_MESSAGE = 3;
+  int PLAIN_MESSAGE = -1;
+  int DEFAULT_OPTION = -1;
+  int YES_NO_OPTION = 0;
+  int YES_NO_CANCEL_OPTION = 1;
+  int OK_CANCEL_OPTION = 2;
+  int YES_OPTION = 0;
+  int NO_OPTION = 1;
+  int CANCEL_OPTION = 2;
+  int OK_OPTION = 0;
 
-  public static final String CHECK_BOX_MENU_ITEM = "org.diylc.checkBoxMenuItem";
-  public static final String RADIO_BUTTON_GROUP_KEY = "org.diylc.radioButtonGroup";
+  String CHECK_BOX_MENU_ITEM = "org.diylc.checkBoxMenuItem";
+  String RADIO_BUTTON_GROUP_KEY = "org.diylc.radioButtonGroup";
 
   void showMessage(String message, String title, int messageType);
 
