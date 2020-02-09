ceccbaacfa25aa75d2eac319165fb2c5a791082c
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-09 17:05:28 +0200

    added a default icon in code in case resource isn't there - debatable whether it's really needed - oh, and also style fixes

--- src/main/java/org/diylc/components/misc/Image.java
+++ src/main/java/org/diylc/components/misc/Image.java
@@ -17,10 +17,11 @@
   You should have received a copy of the GNU General Public License
   along with DIYLC. If not, see <http://www.gnu.org/licenses/>.
 */
+
 package org.diylc.components.misc;
 
 import com.thoughtworks.xstream.annotations.XStreamConverter;
-import java.awt.AlphaComposite;
+
 import java.awt.Composite;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -28,8 +29,11 @@ import java.awt.Shape;
 import java.awt.geom.Rectangle2D;
 import java.io.IOException;
 import javax.swing.ImageIcon;
+
 import org.apache.poi.util.IOUtils;
+
 import org.diylc.appframework.miscutils.IconImageConverter;
+import org.diylc.common.Config;
 import org.diylc.common.ObjectCache;
 import org.diylc.components.AbstractTransparentComponent;
 import org.diylc.core.ComponentState;
@@ -54,32 +58,38 @@ import org.diylc.core.annotations.PercentEditor;
 public class Image extends AbstractTransparentComponent<Void> {
 
   private static final long serialVersionUID = 1L;
-  public static String DEFAULT_TEXT = "Double click to edit text";
-  private static ImageIcon ICON;
-  private static byte DEFAULT_SCALE = 25;
+  private static final ImageIcon ICON;
+  private static final byte DEFAULT_SCALE = 25;
+
+  public static final String DEFAULT_TEXT = Config.getString("components.double-click-to-edit");
+
+  private Point point = new Point(0, 0);
+  private byte[] data;
+  private Byte scale;
+  private byte newScale = DEFAULT_SCALE;
+  @XStreamConverter(IconImageConverter.class)
+  @Deprecated
+  private ImageIcon image;
 
   static {
-    String name = "/org/diylc/images/image.png";
+    String name = "/org/diylc/images/image-icon.png";
     java.net.URL imgURL = Image.class.getResource(name);
     if (imgURL != null) {
       ICON = new ImageIcon(imgURL, name);
+    } else {
+      byte[] iconData = java.util.Base64.getDecoder().decode("iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAK3RFWHRDcmVhdGlvbiBUaW1lAE1vIDI5IFNlcCAyMDAzIDE1OjE1OjUyICswMTAwvjqHlwAAAAd0SU1FB9YFFw0yKjOOuzAAAAAJcEhZcwAACvAAAArwAUKsNJgAAAAEZ0FNQQAAsY8L/GEFAAACMElEQVR42p1Tz2sTURD+3u4GN92aEKUVizWJYKSHGnKr8T8QQRDBm/ZP8NS7/0EF0YtCjx5qLz0JQgteRHptkZZWsM3GtptEbX7sZnffc+ZlY3LUDjv7fs7MN9/MExiIwPlEieXNZt8WMqWkoqWij0Yp+Qy8p5X3YtpWUt8J/ABLj28V6NKxRcbiyUIGv/0IQgloP9o19DzWa5HMBdIpgbWNGkd/RLpqKYrWJuP1HYnvX7cwV7kLFQdkFZNDSabqrzKScjGTIMQsqW0wJBYnbqA0O4Uv75cRuNvImV1kjA4yZo9Gmos2sqTkFYmNoX86R5pkTYra9VCtVtE43Eb0sw7HCHG1/gk5/wgTlsSk6AxSTIKyWJqUsI8jbwuV6TKUFaM4swCz10J57R6MbkOnc3LnOdwb9zVB4w50Cn2EeNlaxIvdJXjNPVwmAm8fvIVx6SJlehPIz+Hzt1eovKvAC1rEwaiOlmSahcB8KQ9f7uPZzlPsfWhjMTeDN/kSTGcCH5s1PDipAxGXxxySOEohIh9nQUD8pFEsTsOZcrBy4GJl14VjpdDphyCQIKiIdYuMccBljIiZU4Jmqh78KEKgQhSu2XBtH51TsvTpJnEMKkJIpVXjCJgQ50IWr8v7uqOZZT6W3FTEGHdCJAeoOd3JdI4AeCMHQbcXr2/WLDWEpiMkbS1Hra2SNeQPhGe/2FYOH9F10oekhWT9Lw+LjQ+5lfmyTXolGf9HmJnjYbRzP+c/c5s3Rp+EAfUAAAAASUVORK5CYII=");
+      ICON = new ImageIcon(iconData);
     }
+  }
+
+  {
+    image = new ImageIcon(ICON.getImage());
   }
 
   @Override
   public String getControlPointNodeName(int index) {
     return null;
   }
-
-  private Point point = new Point(0, 0);
-
-  @XStreamConverter(IconImageConverter.class)
-  @Deprecated
-  private ImageIcon image;
-
-  private byte[] data;
-  private Byte scale;
-  private byte newScale = DEFAULT_SCALE;
 
   public Image() {
     try {
@@ -106,15 +116,15 @@ public class Image extends AbstractTransparentComponent<Void> {
             getImage().getIconHeight() * s))) {
       return;
     }
-    Composite oldComposite = g2d.getComposite();
-    if (alpha < MAX_ALPHA) {
-      g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f * alpha / MAX_ALPHA));
-    }
 
+    Composite oldComposite = setTransparency(g2d);
     g2d.scale(s, s);
-    g2d.drawImage(getImage().getImage(), (int) (point.x / s), (int) (point.y / s), null);
-    if (componentState == ComponentState.SELECTED) {
-      g2d.setComposite(oldComposite);
+    int x = (int) (point.x / s);
+    int y = (int) (point.y / s);
+    g2d.drawImage(getImage().getImage(), x, y, null);
+    g2d.setComposite(oldComposite);
+
+    if (isSelected()) {
       g2d.scale(1 / s, 1 / s);
       g2d.setColor(SELECTION_COLOR);
       g2d.setStroke(ObjectCache.getInstance().fetchBasicStroke(1f));
@@ -161,8 +171,8 @@ public class Image extends AbstractTransparentComponent<Void> {
       /*
            REMOVED for use of sun.awt.image
 
-           Any old files requiring this could be e.g. converted
-           with an external utility //ola 20191220
+           TODO: Figure out how - and at which stage - to handle old
+           files containing images //ola 20191220
 
          // when loading old files, convert the stored image to
          // byte array and then then discard it, we won't be
@@ -178,10 +188,9 @@ public class Image extends AbstractTransparentComponent<Void> {
          }
          // don't save back to the file
          */
-      image = null;
     }
 
-    return new ImageIcon(data);
+    return image;
   }
 
   @EditableProperty(name = "Image")
@@ -191,7 +200,7 @@ public class Image extends AbstractTransparentComponent<Void> {
 
   public void setData(byte[] data) {
     this.data = data;
-    this.image = null;
+    this.image = new ImageIcon(data);
   }
 
   @PercentEditor(_100PercentValue = 25)
