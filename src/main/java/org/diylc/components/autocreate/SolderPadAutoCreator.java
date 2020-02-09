b11e48d7e5ddc3b4b767872b7ca9ed63ee3a06ca
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-09 10:45:54 +0200

    using convenience funcs in App for config

--- src/main/java/org/diylc/components/autocreate/SolderPadAutoCreator.java
+++ src/main/java/org/diylc/components/autocreate/SolderPadAutoCreator.java
@@ -22,9 +22,12 @@ package org.diylc.components.autocreate;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.diylc.appframework.miscutils.ConfigurationManager;
+
+import org.diylc.App;
+import org.diylc.common.IPlugInPort;
 import org.diylc.components.connectivity.SolderPad;
 import org.diylc.core.IDIYComponent;
 import org.diylc.core.annotations.IAutoCreator;
@@ -33,29 +36,23 @@ public class SolderPadAutoCreator implements IAutoCreator {
 
   private static final Logger LOG = LogManager.getLogger(SolderPadAutoCreator.class);
 
-  public static final String AUTO_PADS_KEY = "autoCreatePads";
-
   @Override
   public List<IDIYComponent<?>> createIfNeeded(IDIYComponent<?> lastAdded) {
-    if (ConfigurationManager.getInstance().readBoolean(AUTO_PADS_KEY, false)
-        && !(lastAdded.getClass().equals(SolderPad.class))) {
-      List<IDIYComponent<?>> res = new ArrayList<IDIYComponent<?>>();
+    List<IDIYComponent<?>> res = null;
+    if (App.autoPads() && !(lastAdded instanceof SolderPad)) {
+      res = new ArrayList<IDIYComponent<?>>();
       for (int i = 0; i < lastAdded.getControlPointCount(); i++) {
         if (lastAdded.isControlPointSticky(i)) {
           try {
             SolderPad pad = new SolderPad();
-            // instantiationManager.instantiateComponent(padType, null,
-            // component.getControlPoint(i), currentProject)
-            // .get(0);
             pad.setControlPoint(lastAdded.getControlPoint(i), 0);
             res.add(pad);
           } catch (Exception e) {
-            LOG.warn("Could not auto-create solder pad", e);
+            LOG.error("Could not auto-create solder pad", e);
           }
         }
       }
-      return res;
     }
-    return null;
+    return res;
   }
 }
