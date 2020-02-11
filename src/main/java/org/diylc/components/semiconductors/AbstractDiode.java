a23df939855f3ef7218098514768eddc07071b65
Ola Rinta-Koski <github@rinta-koski.net>
2020-02-11 11:31:08 +0200

    superclass for common functionality in Diode{Glass,Plastic}

---
+++ src/main/java/org/diylc/components/semiconductors/AbstractDiode.java
@@ -0,0 +1,76 @@
+/*
+  DIY Layout Creator (DIYLC).
+  Copyright (c) 2009-2018 held jointly by the individual authors.
+
+  This file is part of DIYLC.
+
+  DIYLC is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  DIYLC is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with DIYLC.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package org.diylc.components.semiconductors;
+
+import java.awt.Color;
+import org.diylc.components.AbstractLeadedComponent;
+import org.diylc.components.Area;
+import org.diylc.core.annotations.EditableProperty;
+
+public abstract class AbstractDiode extends AbstractLeadedComponent<String> {
+
+  protected String value = "";
+  protected Color markerColor;
+
+  protected AbstractDiode(Color markerColor) {
+    super();
+    this.markerColor = markerColor;
+  }
+
+  @Override
+  protected boolean supportsStandingMode() {
+    return true;
+  }
+
+  @Override
+  public Color getStandingBodyColor() {
+    return getFlipStanding() ? getBodyColor() : getMarkerColor();
+  }
+
+  @EditableProperty(name = "Reverse (standing)")
+  public boolean getFlipStanding() {
+    return super.getFlipStanding();
+  }
+
+  @EditableProperty
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  @EditableProperty(name = "Marker")
+  public Color getMarkerColor() {
+    return markerColor;
+  }
+
+  public void setMarkerColor(Color markerColor) {
+    this.markerColor = markerColor;
+  }
+
+  @Override
+  protected Area getBodyShape() {
+    return Area.rect(
+        0f, 0f, getLength().convertToPixels(), getClosestOdd(getWidth().convertToPixels()));
+  }
+}
